b0e52d67a508b54ebba06098e0e9cec0f1decac4
naimur-reza <naimur.rezaa@gmail.com>
2024-05-23 15:45:55 +0600

    add

---
+++ src/lib/Providers/Providers.tsx
@@ -0,0 +1,9 @@
+import { TChildren } from "@/types/common";
+import { ThemeProvider } from "@mui/material";
+import theme from "../theme";
+
+const Providers = ({ children }: TChildren) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+export default Providers;
---
+++ src/lib/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff0000",
+    },
+    secondary: {
+      main: "#00ff00",
+    },
+  },
+});
+
+export default theme;
